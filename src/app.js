68a9854d0a9e2326d996922bc3b0295c5902fe3c
Drake Nguyen <drakeknguyen@yahoo.com>
2022-08-08 23:11:26 -0700

    Updated screen panel and display to be more responsive in large mobile displays

--- src/app.js
+++ src/app.js
@@ -1,10 +1,26 @@
-const greeting = 'Hello World';
+const greeting = "Hello World";
 console.log(greeting);
 
-const getData = async (url) => {
-  const response = await fetch(url);
-  const result = await response.json();
-  console.log(result);
-};
+const onClick = ()=> {
+  console.log("attack!")
+}
 
-getData('https://jsonplaceholder.typicode.com/posts');
+const attackBtn = document.getElementById("btn--attack");
+console.log(attackBtn)
+// const attackBtn = document.querySelector(".btn--attack");
+attackBtn.addEventListener("click", onClick);
+// attackBtn.addEventListener("click", () => {
+//   console.log("attack!");
+// });
+
+// document.getElementsById("btn btn--attack").addEventListener("click", ()=> onClick)
+
+
+
+// const getData = async (url) => {
+//   const response = await fetch(url);
+//   const result = await response.json();
+//   console.log(result);
+// };
+
+// getData('https://jsonplaceholder.typicode.com/posts');
