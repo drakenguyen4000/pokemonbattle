a4d29c031547dd2482ef73fc52dfe2847537e822
Drake Nguyen <drakeknguyen@yahoo.com>
2022-08-17 23:41:53 -0700

    update screen pages in iframe

--- src/app.js
+++ src/app.js
@@ -1,26 +1,10 @@
-const greeting = "Hello World";
-console.log(greeting);
 
-const onClick = ()=> {
-  console.log("attack!")
-}
-
-const attackBtn = document.getElementById("btn--attack");
-console.log(attackBtn)
-// const attackBtn = document.querySelector(".btn--attack");
-attackBtn.addEventListener("click", onClick);
-// attackBtn.addEventListener("click", () => {
-//   console.log("attack!");
-// });
-
-// document.getElementsById("btn btn--attack").addEventListener("click", ()=> onClick)
+//Intro start game button
+document.querySelector(".btn-start").addEventListener("click", function () {
+  //Take Intro Screen
+  // const page = 'selection-screen.html';
+  const page = 'battle-screen.html';
+  document.getElementsByName('screen-display')[0].src = page;
+});
 
 
-
-// const getData = async (url) => {
-//   const response = await fetch(url);
-//   const result = await response.json();
-//   console.log(result);
-// };
-
-// getData('https://jsonplaceholder.typicode.com/posts');
