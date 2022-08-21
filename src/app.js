d76304e70d2c12ee3eb21e5560f9e5b31b3c0d5f
Drake Nguyen <drakeknguyen@yahoo.com>
2022-08-20 21:23:25 -0700

    refactored functionality to d-pad in selecting screen

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ let iframe;
 let parent;
 let children;
 let numItems; //Number of Pokemon available to select
-var count = 0;
+let count = 0;
 
 //Start Game
 startButton.addEventListener("click", () => {
@@ -24,7 +24,8 @@ async function getIframe() {
   iframe = document.getElementById("iframe");
   parent = iframe.contentWindow.document.querySelector(".selection__list");
   numItems =
-    iframe.contentWindow.document.querySelector(".selection__list").children.length - 1;
+    iframe.contentWindow.document.querySelector(".selection__list").children
+      .length - 1;
   children = parent.children;
 }
 
@@ -32,51 +33,55 @@ async function getIframe() {
 //Right button moves selection border one over to the right
 rightButton.addEventListener("click", () => {
   count = count + 1;
-  if (count > numItems) {
-    count = 8;
-  } else {
-    //removes selection border from previous selection
-    children[count -1].classList.remove("selection__card--border");
-    //adds selection border to current selection
-    children[count].classList.add("selection__card--border");
-  }
+  //Set equal to # of selections available, if count exceeds it, before changing direction
+  count > numItems ? (count = numItems) : switchDirection("right");
 });
 
 //Left button moves selection border one over to the left
 leftButton.addEventListener("click", () => {
   count = count - 1;
-  if (count < 0) {
-    count = 0;
-  } else {
-    //removes selection border from previous selection
-    children[count +1].classList.remove("selection__card--border");
-    //adds selection border to current selection
-    children[count].classList.add("selection__card--border");
-  }
+  //Set count equal to zero, if count goes below zero, before changing direction
+  count < 0 ? (count = 0) : switchDirection("left");
 });
 
 //Down button moves selection border one below
 downButton.addEventListener("click", () => {
   count = count + 3;
-  if (count > numItems) {
-    count = count - 3;
-  } else {
-    // //removes selection border from previous selection
-    children[count - 3].classList.remove("selection__card--border");
-    //adds selection border to current selection
-    children[count].classList.add("selection__card--border");
-  }
+  //Reverse count by -3, if count exceeds # of selections available, before changing direction
+  count > numItems ? (count = count - 3) : switchDirection("down");
 });
 
 //Up button moves selection border one above
 upButton.addEventListener("click", () => {
   count = count - 3;
-  if (count < 0) {
-    count = count + 3;
-  } else {
-    //removes selection border from previous selection
-    children[count + 3].classList.remove("selection__card--border");
-    //adds selection border to current selection
-    children[count].classList.add("selection__card--border");
-  }
+  //Reverse count by +3, if count goes below zero, before changing direction
+  count < 0 ? (count = count + 3) : switchDirection("up");
 });
+
+const switchDirection = (direction) => {
+  switch (direction) {
+    case "right":
+      //removes selection border from previous selection
+      children[count - 1].classList.remove("selection__card--border");
+      //adds selection border to current selection
+      children[count].classList.add("selection__card--border");
+      break;
+    case "left":
+      //removes selection border from previous selection
+      children[count + 1].classList.remove("selection__card--border");
+      //adds selection border to current selection
+      children[count].classList.add("selection__card--border");
+      break;
+    case "down":
+      //removes selection border from previous selection
+      children[count - 3].classList.remove("selection__card--border");
+      //adds selection border to current selection
+      children[count].classList.add("selection__card--border");
+      break;
+    case "up":
+      //removes selection border from previous selection
+      children[count + 3].classList.remove("selection__card--border");
+      //adds selection border to current selection
+      children[count].classList.add("selection__card--border");
+  }
+};
