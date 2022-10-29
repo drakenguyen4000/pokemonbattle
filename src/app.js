c05a18296744e636eca77b5c1ace576a49a7da5d
Drake Nguyen <drakeknguyen@yahoo.com>
2022-10-29 14:52:51 -0700

    revised tackle as third attack

--- src/app.js
+++ src/app.js
@@ -58,19 +58,19 @@ var state = {
 };
 
 const sound = {
-  // selecting: new Audio("./src/sounds/171697__nenadsimic__menu-selection-click.wav"),
   click: new Audio("./src/sounds/splits.mp3"),
   click2: new Audio("./src/sounds/608432__plasterbrain__pokemon-ui-select-enter.flac"),
   intro: new Audio("./src/sounds/514155__edwardszakal__game-music.mp3"),
-  energyBlast: new Audio("./src/sounds/213149__complex-waveform__8bit-style-bonus-effect.wav"),
+  attack: new Audio("./src/sounds/213149__complex-waveform__8bit-style-bonus-effect.wav"),
   heal: new Audio("./src/sounds/562292__colorscrimsontears__heal-rpg.wav"),
+  smash: new Audio("./src/sounds/323417__sethroph__glass-slide-7.wav"),
   catch: new Audio("./src/sounds/464904__plasterbrain__8bit-beam.flac"),
   fail: new Audio("./src/sounds/159408__noirenex__life-lost-game-over.wav"),
   winGame: new Audio("./src/sounds/win-video-game-sound.wav"),
   lostGame: new Audio("./src/sounds/538151__fupicat__8bit-fall.wav"),
   gameOver: new Audio("./src/sounds/617466__cwright13__pokemoncenter.mp3"),
   battle: new Audio("./src/sounds/338817__sirkoto51__rpg-battle-loop-1.wav"),
-  // click: new Audio("./src/sounds/338817__sirkoto51__rpg-battle-loop-1.wav"),
+  switch: new Audio("./src/sounds/274180__littlerobotsoundfactory__jingle-win-synth-00.wav"),
 };
 
 /*Displays*/
@@ -223,16 +223,15 @@ startButton.addEventListener("click", () => {
 });
 
 pauseButton.addEventListener("click", () => {
-  sound.click2.play();
   // oppTurn();
-  // console.log("state:", state);
-  // console.log("count:", count);
-  // console.log("count2:", count2);
-  // console.log("count3:", count3);
-  // console.log("numItems:", numItems);
-  // console.log("numItems2:", numItems2);
-  // console.log("numItems3:", numItems3);
-  // console.log(state);
+  console.log("state:", state);
+  console.log("count:", count);
+  console.log("count2:", count2);
+  console.log("count3:", count3);
+  console.log("numItems:", numItems);
+  console.log("numItems2:", numItems2);
+  console.log("numItems3:", numItems3);
+  console.log(state);
 });
 
 selectButton.addEventListener("click", () => {
@@ -241,7 +240,6 @@ selectButton.addEventListener("click", () => {
   if (
     state.screen === "selected-mode" &&
     state.selectedAnswer === "yes"
-    // && state.optionSelected === "attack"
   ) {
     //Pokemon team selection. Player cannot select same Pokemon on team.
     if (state.yourPkmn.find((e) => e.name === state.curSelectPokemon.name)) {
@@ -308,11 +306,6 @@ selectButton.addEventListener("click", () => {
 
 rightButton.addEventListener("click", () => {
   sound.click.play();
-  //Prevents count / d-pad change
-  // if (state.screen !== "hold-mode" && state.screen !== "gameover-screen") {
-  //   //Adds 1 every time user clicks right button on d-pad
-  //   count += 1;
-  // }
   //Only works in selection screen, not selected-mode
   if (state.screen === "selection-screen") {
     count += 1;
@@ -324,12 +317,9 @@ rightButton.addEventListener("click", () => {
     count += 1;
     //Set equal to # of selections available, if count exceeds it, before changing direction
     count > numItems ? (count = numItems) : switchDirection2("right");
-  // } else if (state.screen === "attack-mode") {  //old attack option layout
-  //   count += 1;
-  //   //Set equal to # of selections available, if count exceeds it, before changing direction
-  //   count > 1 ? (count = 1) : switchDirection2("right");
   } else if (state.screen === "yourPkmn") {
     count3 += 1;
+  //Set equal to # of selections available, if count exceeds it, before changing direction
     count3 > numItems3 ? (count3 = numItems3) : switchDirection5("right");
   }
 });
@@ -337,15 +327,11 @@ rightButton.addEventListener("click", () => {
 //Left button moves selection border one over to the left
 leftButton.addEventListener("click", () => {
   sound.click.play();
-  // if (state.screen !== "hold-mode" && state.screen !== "gameover-screen") {
-  //   count -= 1;
-  // }
   if (state.screen === "selection-screen") {
     count -= 1;
     //Set count equal to zero, if count goes below zero, before changing direction
     count < 0 ? (count = 0) : switchDirection("left");
   }
-  // if (state.screen === "battle-screen" || state.screen === "attack-mode") { //old attack option layout
   if (state.screen === "battle-screen") {
     count -= 1;
     //Set count equal to zero, if count goes below zero, before changing direction
@@ -373,7 +359,7 @@ downButton.addEventListener("click", () => {
     count > numItems ? (count -= 2) : switchDirection2("down");
   } else if (state.screen === "attack-mode") {
     count += 1; 
-    count > 1 ? (count = 1) : switchDirection6("down");
+    count > 2 ? (count = 2) : switchDirection6("down");
   } else if (state.screen === "gameover-screen") {
     count += 1;
     count > numItems ? (count = numItems) : switchDirection3("down");
@@ -466,7 +452,6 @@ const switchDirection2 = (direction) => {
 
       //Update state with player choice
       state.optionSelected = optionsListItems[count].lastChild.data;
-      // state.attackSelected = count > 0 ? "attack_2" : "attack_1"; //old attack option layout
       break;
     case "left":
       optionsListItems[count + 1].children[0].classList.remove(
@@ -474,7 +459,6 @@ const switchDirection2 = (direction) => {
       );
       optionsListItems[count].children[0].classList.add("arrow--selected");
       state.optionSelected = optionsListItems[count].lastChild.data;
-      // state.attackSelected = count > 0 ? "attack_2" : "attack_1"; //old attack option layout
       break;
     case "down":
       optionsListItems[count - 2].children[0].classList.remove(
@@ -572,9 +556,9 @@ const switchDirection6 = (direction) => {
         "arrow--selected"
       );
       optionsListItems[count].children[0].classList.add("arrow--selected");
-      // //Update state with player choice
+      //Update state with player choice
       state.optionSelected = optionsListItems[count].lastChild.data;
-      state.attackSelected = count > 0 ? "attack_2" : "attack_1";
+      state.attackSelected = `attack_${count + 1}`;
       break;
     case "up":
       optionsListItems[count + 1].children[0].classList.remove(
@@ -582,13 +566,13 @@ const switchDirection6 = (direction) => {
       );
       optionsListItems[count].children[0].classList.add("arrow--selected");
       state.optionSelected = optionsListItems[count].lastChild.data;
-      state.attackSelected = count > 0 ? "attack_2" : "attack_1";
+      state.attackSelected = `attack_${count + 1}`;
       break;
   }
 };
 
 const attackOption = () => {
-  // //Set Attack Option Grid layout as list
+  //Set Attack Option Grid layout as list
   iframeDocument.querySelector(".option-grid").classList.add("option-grid--list");
   if (state.optionSelected === "attack") {
     state.screen = "attack-mode";
@@ -600,7 +584,7 @@ const attackOption = () => {
       state.yourPkmn[state.curPkmIndex].attack_1;
     optionsListItems[1].lastChild.data =
       state.yourPkmn[state.curPkmIndex].attack_2;
-    optionsListItems[2].lastChild.data = "";
+    optionsListItems[2].lastChild.data = state.yourPkmn[state.curPkmIndex].attack_3;
     optionsListItems[3].lastChild.data = "";
   } else if (state.screen === "attack-mode" && state.screen !== "hold-mode") {
     //Displays user command in infobox yellow
@@ -615,13 +599,13 @@ const attackOption = () => {
         iframeDocument
           .querySelector(".player__img")
           .classList.add("player-attack");
-        sound.energyBlast.play();
+        sound.attack.play();
         iframeDocument
           .querySelector(".player-energy")
           .classList.add(`player-energy--show`, `player-${energy}--animate`);
         iframeDocument
           .querySelector(".opponent__img")
-          .classList.add("staggered_2");
+          .classList.add("opponent--stagger");
         setTimeout(() => {
           iframeDocument
             .querySelector(".player__img")
@@ -631,49 +615,71 @@ const attackOption = () => {
             .classList.remove(`player-energy--show`, `player-${energy}--animate`);
           iframeDocument
             .querySelector(".opponent__img")
-            .classList.remove("staggered_2");
+            .classList.remove("opponent--stagger");
           let attack = Math.floor(playerPokemon.attackPower("attack_1"));
           oppPokemon.damage(attack);
           iframe.contentWindow.updateValues();
           oppTurn();
-          resetOptions();
         }, 4000);
       }, 1000);
     }
     if (state.attackSelected === "attack_2") {
       const energy = state.yourPkmn[state.curPkmIndex].attack_2;
-      sound.energyBlast.play();
+      sound.attack.play();
       setTimeout(() => {
         iframeDocument
           .querySelector(".player__img")
           .classList.add("player-attack_2");
-        // iframeDocument
-        //   .querySelector(".player__img")
-        //   .classList.add(`player-${energy}--animate`);
         iframeDocument
           .querySelector(".player-energy")
           .classList.add(`player-energy--show`, `player-${energy}--animate`);
         iframeDocument
           .querySelector(".opponent__img")
-          .classList.add("staggered_2");
+          .classList.add("opponent--stagger");
         setTimeout(() => {
           iframeDocument
             .querySelector(".player__img")
             .classList.remove("player-attack_2");
-          // iframeDocument
-          //   .querySelector(".player__img")
-          //   .classList.remove(`player-${energy}--animate`);
           iframeDocument
             .querySelector(".player-energy")
             .classList.remove(`player-energy--show`, `player-${energy}--animate`);
           iframeDocument
             .querySelector(".opponent__img")
-            .classList.remove("staggered_2");
+            .classList.remove("opponent--stagger");
           let attack = Math.floor(playerPokemon.attackPower("attack_2"));
           oppPokemon.damage(attack);
           iframe.contentWindow.updateValues();
           oppTurn();
-          resetOptions();
+        }, 4000);
+      }, 1000);
+    }
+    if (state.attackSelected === "attack_3") {
+      const energy = state.yourPkmn[state.curPkmIndex].attack_3;
+      sound.smash.play();
+      setTimeout(() => {
+        iframeDocument
+          .querySelector(".player__img")
+          .classList.add("player-attack_3");
+        iframeDocument
+          .querySelector(".player-energy")
+          .classList.add(`player-energy--show`, `player-${energy}--animate`);
+        iframeDocument
+          .querySelector(".opponent__img")
+          .classList.add("opponent--stagger");
+        setTimeout(() => {
+          iframeDocument
+            .querySelector(".player__img")
+            .classList.remove("player-attack_3");
+          iframeDocument
+            .querySelector(".player-energy")
+            .classList.remove(`player-energy--show`, `player-${energy}--animate`);
+          iframeDocument
+            .querySelector(".opponent__img")
+            .classList.remove("opponent--stagger");
+          let attack = Math.floor(playerPokemon.attackPower("attack_3"));
+          oppPokemon.damage(attack);
+          iframe.contentWindow.updateValues();
+          oppTurn();
         }, 4000);
       }, 1000);
     }
@@ -761,6 +767,8 @@ const pkmonOption = () => {
       state.yourPkmn[state.curPkmIndex].name
     }!`;
     loadPokemon();
+    sound.switch.play();
+    sound.switch.volume = .4;
     playerPokemon.switchPkmnHeatlh();
     setTimeout(() => {
       oppTurn();
@@ -792,7 +800,6 @@ const closeBag = () => {
   iframeDocument.querySelector(".bag").classList.remove("bag--show");
   bagItems[count2].classList.remove("bag-item--selected");
   bagItems[count2].children[1].classList.remove("bag-item-img--selected");
-  //reset options
   state.optionSelected = "bag";
   count2 = 0;
   bagItems[count2].classList.add("bag-item--selected");
@@ -851,17 +858,18 @@ const oppTurn = () => {
   }
   setTimeout(() => {
     if (state.screen !== "gameover-screen") {
-      state.attackSelected = Math.random() > 0.5 ? "attack_2" : "attack_1";
+      const ranNum = Math.floor(Math.random() * 3) + 1;
+      state.attackSelected =  `attack_${ranNum}`;
       if (state.attackSelected === "attack_1") {
         const energy = state.opponentPokemon.attack_1;
-        sound.energyBlast.play();
+        sound.attack.play();
         iframeDocument
           .querySelector(".opponent__img")
           .classList.add("opponent-attack");
         iframeDocument
           .querySelector(".opponent-energy")
           .classList.add(`opponent-energy--show`, `opponent-${energy}--animate`);
-        iframeDocument.querySelector(".player__img").classList.add("staggered");
+        iframeDocument.querySelector(".player__img").classList.add("player--stagger");
         setTimeout(() => {
           iframeDocument
             .querySelector(".opponent__img")
@@ -871,7 +879,7 @@ const oppTurn = () => {
             .classList.remove(`opponent-energy--show`, `opponent-${energy}--animate`);
           iframeDocument
             .querySelector(".player__img")
-            .classList.remove("staggered");
+            .classList.remove("player--stagger");
           let attack = Math.floor(oppPokemon.attackPower("attack_1"));
           playerPokemon.damage(attack);
           resetOptions();
@@ -879,7 +887,7 @@ const oppTurn = () => {
       }
       if (state.attackSelected === "attack_2") {
         const energy = state.opponentPokemon.attack_2;
-        sound.energyBlast.play();
+        sound.attack.play();
         iframeDocument
           .querySelector(".opponent__img")
           .classList.add("opponent-attack_2");
@@ -888,7 +896,7 @@ const oppTurn = () => {
           .classList.add(`opponent-energy--show`, `opponent-${energy}--animate`);
         iframeDocument
           .querySelector(".player__img")
-          .classList.add("staggered");
+          .classList.add("player--stagger");
         setTimeout(() => {
           iframeDocument
             .querySelector(".opponent__img")
@@ -898,8 +906,35 @@ const oppTurn = () => {
             .classList.remove(`opponent-energy--show`, `opponent-${energy}--animate`);
           iframeDocument
             .querySelector(".player__img")
-            .classList.remove("staggered");
+            .classList.remove("player--stagger");
           let attack = Math.floor(oppPokemon.attackPower("attack_2"));
+          playerPokemon.damage(attack);
+          resetOptions();
+        }, 4000);
+      }
+      if (state.attackSelected === "attack_3") {
+        const energy = state.opponentPokemon.attack_3;
+        sound.smash.play();
+        iframeDocument
+          .querySelector(".opponent__img")
+          .classList.add("opponent-attack_3");
+        iframeDocument
+          .querySelector(".opponent-energy")
+          .classList.add(`opponent-energy--show`, `opponent-${energy}--animate`);
+        iframeDocument
+          .querySelector(".player__img")
+          .classList.add("player--stagger");
+        setTimeout(() => {
+          iframeDocument
+            .querySelector(".opponent__img")
+            .classList.remove("opponent-attack_3");
+          iframeDocument
+            .querySelector(".opponent-energy")
+            .classList.remove(`opponent-energy--show`, `opponent-${energy}--animate`);
+          iframeDocument
+            .querySelector(".player__img")
+            .classList.remove("player--stagger");
+          let attack = Math.floor(oppPokemon.attackPower("attack_3"));
           playerPokemon.damage(attack);
           resetOptions();
         }, 4000);
@@ -987,7 +1022,7 @@ class Pokemon {
   }
   attackPower(attacktype) {
     let accuracy = Math.random() * (1 - 0.5) + 0.5;
-    if (attacktype === "attack_1") {
+    if (attacktype === "attack_1" || attacktype === "attack_3") {
       return (
         this.pkmState.health_active * 0.05 + this.attackpt * 0.22 * accuracy
       );
